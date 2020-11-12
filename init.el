fc6568df67d0c4dba3d39805c94debecab34c536
poisson_myfish <myfishispoisson@gmail.com>
2020-11-12 10:37:06 +0200

    Initial Commit

---
+++ init.el
@@ -0,0 +1,132 @@
+;; Melpa
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("4cf9ed30ea575fb0ca3cff6ef34b1b87192965245776afa9e9e20c17d115f3fb" "aded61687237d1dff6325edb492bde536f40b048eab7246c61d5c6643c696b7f" "5f824cddac6d892099a91c3f612fcf1b09bb6c322923d779216ab2094375c5ee" default))
+ '(package-selected-packages
+   '(projectile color-identifiers-mode hl-todo gruvbox-theme markdown-mode+ haskell-mode haskell-emacs gruber-darker-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
+;; Theme and font
+;;(load-theme 'gruber-darker)
+(load-theme 'gruvbox-dark-hard)  ;; Flavors: dark-soft, dark-medium, dark-hard
+(add-to-list 'default-frame-alist
+	     '(font . "Ubuntu Mono-12"))
+
+;; Menu bar and stuff
+(menu-bar-mode -1)
+(toggle-scroll-bar -1)
+(tool-bar-mode -1)
+
+;; Tab size
+(setq-default c-basic-offset 4)
+(setq-default indent-tabs-mode nil)
+
+
+;; ======================================= ;;
+;; == Some tabbing stuff ================= ;;
+;; ======================================= ;;
+
+(defun indent-region-custom(numSpaces)
+    (progn 
+        ; default to start and end of current line
+        (setq regionStart (line-beginning-position))
+        (setq regionEnd (line-end-position))
+
+        ; if there's a selection, use that instead of the current line
+        (when (use-region-p)
+            (setq regionStart (region-beginning))
+            (setq regionEnd (region-end))
+        )
+
+        (save-excursion ; restore the position afterwards            
+            (goto-char regionStart) ; go to the start of region
+            (setq start (line-beginning-position)) ; save the start of the line
+            (goto-char regionEnd) ; go to the end of region
+            (setq end (line-end-position)) ; save the end of the line
+
+            (indent-rigidly start end numSpaces) ; indent between start and end
+            (setq deactivate-mark nil) ; restore the selected region
+        )
+    )
+)
+
+(defun untab-region (N)
+    (interactive "p")
+    (indent-region-custom -4)
+)
+
+(defun tab-region (N)
+    (interactive "p")
+    (if (active-minibuffer-window)
+        (minibuffer-complete)    ; tab is pressed in minibuffer window -> do completion
+    ; else
+    (if (string= (buffer-name) "*shell*")
+        (comint-dynamic-complete) ; in a shell, use tab completion
+    ; else
+    (if (use-region-p)    ; tab is pressed is any other buffer -> execute with space insertion
+        (indent-region-custom 4) ; region was selected, call indent-region
+        (insert "    ") ; else insert four spaces as expected
+    )))
+)
+
+(global-set-key (kbd "<backtab>") 'untab-region)
+(global-set-key (kbd "<tab>") 'tab-region)
+
+
+;; ============================================================= ;;
+
+
+;; TODO stuff
+(add-hook 'c++-mode-hook 'hl-todo-mode)
+
+
+;; Better syntax highlighting
+
+(font-lock-add-keywords 'c++-mode
+ `((,(concat
+   "\\<[_a-zA-Z][_a-zA-Z0-9]*\\>"       ; Object identifier
+   "\\s *"                              ; Optional white space
+   "\\(?:\\.\\|->\\)"                   ; Member access
+   "\\s *"                              ; Optional white space
+   "\\<\\([_a-zA-Z][_a-zA-Z0-9]*\\)\\>" ; Member identifier
+   "\\s *"                              ; Optional white space
+   "(")                                 ; Paren for method invocation
+   1 'font-lock-function-name-face t)))
+
+(font-lock-add-keywords 'c++-mode
+ `((,(concat
+   "\\<[_a-zA-Z][_a-zA-Z0-9]*\\>"       ; Object identifier
+   "\\s *"                              ; Optional white space
+   "\\(?:\\.\\|->\\)"                   ; Member access
+   "\\s *"                              ; Optional white space
+   "\\<\\([_a-zA-Z][_a-zA-Z0-9]*\\)\\>" ; Member identifier
+   )
+   1 'font-lock-variable-name-face t)))
+
+(font-lock-add-keywords 'c++-mode
+ `((,(concat
+   "\\<\\([_a-zA-Z][_a-zA-Z0-9]*\\)\\>"  ; Member identifier
+   "(")                                  ; Paren for method invocation
+    1 'font-lock-function-name-face t)))
+
+
+
+;; Projectile
+(global-set-key (kbd "M-p") 'projectile-find-file)
